03a72c9bf77d103260d508860182cc86ae08b49e
Dioka Ejionueme <diokajr@gmail.com>
2022-07-13 21:47:37 +0100

    fix: :bug: images now responsive
    found an efficient way of switching between desktop - mobile images

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const navToggle = document.querySelector('.nav-toggle');
 const nav = document.querySelector('.nav');
 
+/* const changeImage = document.getElementById('deepearth');
+changeImage.src  = "images/mobile/image-deep-earth.jpg"; */
 
 navToggle.addEventListener('click', () => {
     console.log('hello')
